kafi2016/chainer-kafi

Support variable length inputs for LSTM

tests/chainer_tests/functions_tests/activation_tests/test_lstm.py
@@ -16,16 +16,21 @@ def _sigmoid(x):
 
 
 @testing.parameterize(*testing.product({
+    'batch': [3, 2],
     'dtype': [numpy.float16, numpy.float32, numpy.float64],
 }))
 class TestLSTM(unittest.TestCase):
 
     def setUp(self):
-        self.c_prev = numpy.random.uniform(-1, 1, (3, 2, 4)).astype(self.dtype)
-        self.x = numpy.random.uniform(-1, 1, (3, 8, 4)).astype(self.dtype)
+        hidden_shape = (3, 2, 4)
+        x_shape = (self.batch, 8, 4)
+        y_shape = (self.batch, 2, 4)
+        self.c_prev = numpy.random.uniform(
+            -1, 1, hidden_shape).astype(self.dtype)
+        self.x = numpy.random.uniform(-1, 1, x_shape).astype(self.dtype)
 
-        self.gc = numpy.random.uniform(-1, 1, (3, 2, 4)).astype(self.dtype)
-        self.gh = numpy.random.uniform(-1, 1, (3, 2, 4)).astype(self.dtype)
+        self.gc = numpy.random.uniform(-1, 1, hidden_shape).astype(self.dtype)
+        self.gh = numpy.random.uniform(-1, 1, y_shape).astype(self.dtype)
 
         self.check_forward_options = {}
         self.check_backward_options = {'eps': 1e-2}
@@ -46,6 +51,7 @@ class TestLSTM(unittest.TestCase):
         c, h = functions.lstm(c_prev, x)
         self.assertEqual(c.data.dtype, self.dtype)
         self.assertEqual(h.data.dtype, self.dtype)
+        batch = len(x_data)
 
         # Compute expected out
         a_in = self.x[:, [0, 4]]
@@ -54,13 +60,15 @@ class TestLSTM(unittest.TestCase):
         o_in = self.x[:, [3, 7]]
 
         c_expect = _sigmoid(i_in) * numpy.tanh(a_in) + \
-            _sigmoid(f_in) * self.c_prev
+            _sigmoid(f_in) * self.c_prev[:batch]
         h_expect = _sigmoid(o_in) * numpy.tanh(c_expect)
 
         gradient_check.assert_allclose(
-            c_expect, c.data, **self.check_forward_options)
+            c_expect, c.data[:batch], **self.check_forward_options)
         gradient_check.assert_allclose(
             h_expect, h.data, **self.check_forward_options)
+        gradient_check.assert_allclose(
+            c_prev_data[batch:], c.data[batch:], **self.check_forward_options)
 
     @condition.retry(3)
     def test_forward_cpu(self):

tests/chainer_tests/links_tests/connection_tests/test_lstm.py
@@ -28,33 +28,39 @@ class TestLSTM(unittest.TestCase):
         self.upward = upward.copy()  # fixed on CPU
         self.lateral = lateral.copy()  # fixed on CPU
 
-        x_shape = (4, self.in_size)
-        self.x = numpy.random.uniform(-1, 1, x_shape).astype(numpy.float32)
+        x1_shape = (4, self.in_size)
+        self.x1 = numpy.random.uniform(-1, 1, x1_shape).astype(numpy.float32)
+        x2_shape = (3, self.in_size)
+        self.x2 = numpy.random.uniform(-1, 1, x2_shape).astype(numpy.float32)
 
-    def check_forward(self, x_data):
+    def check_forward(self, x1_data, x2_data):
         xp = self.link.xp
-        x = chainer.Variable(x_data)
-        h1 = self.link(x)
-        c0 = chainer.Variable(xp.zeros((len(self.x), self.out_size),
-                                       dtype=self.x.dtype))
-        c1_expect, h1_expect = functions.lstm(c0, self.link.upward(x))
+        x1 = chainer.Variable(x1_data)
+        h1 = self.link(x1)
+        c0 = chainer.Variable(xp.zeros((len(self.x1), self.out_size),
+                                       dtype=self.x1.dtype))
+        c1_expect, h1_expect = functions.lstm(c0, self.link.upward(x1))
         gradient_check.assert_allclose(h1.data, h1_expect.data)
         gradient_check.assert_allclose(self.link.h.data, h1_expect.data)
         gradient_check.assert_allclose(self.link.c.data, c1_expect.data)
 
-        h2 = self.link(x)
+        batch = len(x2_data)
+        x2 = chainer.Variable(x2_data)
+        h2 = self.link(x2)
+        h1_in, h1_rest = functions.split_axis(h1, [batch], axis=0)
         c2_expect, h2_expect = \
             functions.lstm(c1_expect,
-                           self.link.upward(x) + self.link.lateral(h1))
-        gradient_check.assert_allclose(h2.data, h2_expect.data)
+                           self.link.upward(x2) + self.link.lateral(h1_in))
+        gradient_check.assert_allclose(h2.data[:batch], h2_expect.data)
+        gradient_check.assert_allclose(h2.data[batch:], h1_rest.data)
 
     def test_forward_cpu(self):
-        self.check_forward(self.x)
+        self.check_forward(self.x1, self.x2)
 
     @attr.gpu
     def test_forward_gpu(self):
         self.link.to_gpu()
-        self.check_forward(cuda.to_gpu(self.x))
+        self.check_forward(cuda.to_gpu(self.x1), cuda.to_gpu(self.x2))
 
 
 class TestLSSTMRestState(unittest.TestCase):

tsukuba-hpcs/chainer

tests/chainer_tests/functions_tests/activation_tests/test_lstm.py
@@ -16,16 +16,21 @@ def _sigmoid(x):
 
 
 @testing.parameterize(*testing.product({
+    'batch': [3, 2],
     'dtype': [numpy.float16, numpy.float32, numpy.float64],
 }))
 class TestLSTM(unittest.TestCase):
 
     def setUp(self):
-        self.c_prev = numpy.random.uniform(-1, 1, (3, 2, 4)).astype(self.dtype)
-        self.x = numpy.random.uniform(-1, 1, (3, 8, 4)).astype(self.dtype)
+        hidden_shape = (3, 2, 4)
+        x_shape = (self.batch, 8, 4)
+        y_shape = (self.batch, 2, 4)
+        self.c_prev = numpy.random.uniform(
+            -1, 1, hidden_shape).astype(self.dtype)
+        self.x = numpy.random.uniform(-1, 1, x_shape).astype(self.dtype)
 
-        self.gc = numpy.random.uniform(-1, 1, (3, 2, 4)).astype(self.dtype)
-        self.gh = numpy.random.uniform(-1, 1, (3, 2, 4)).astype(self.dtype)
+        self.gc = numpy.random.uniform(-1, 1, hidden_shape).astype(self.dtype)
+        self.gh = numpy.random.uniform(-1, 1, y_shape).astype(self.dtype)
 
         self.check_forward_options = {}
         self.check_backward_options = {'eps': 1e-2}
@@ -46,6 +51,7 @@ class TestLSTM(unittest.TestCase):
         c, h = functions.lstm(c_prev, x)
         self.assertEqual(c.data.dtype, self.dtype)
         self.assertEqual(h.data.dtype, self.dtype)
+        batch = len(x_data)
 
         # Compute expected out
         a_in = self.x[:, [0, 4]]
@@ -54,13 +60,15 @@ class TestLSTM(unittest.TestCase):
         o_in = self.x[:, [3, 7]]
 
         c_expect = _sigmoid(i_in) * numpy.tanh(a_in) + \
-            _sigmoid(f_in) * self.c_prev
+            _sigmoid(f_in) * self.c_prev[:batch]
         h_expect = _sigmoid(o_in) * numpy.tanh(c_expect)
 
         testing.assert_allclose(
-            c_expect, c.data, **self.check_forward_options)
+            c_expect, c.data[:batch], **self.check_forward_options)
         testing.assert_allclose(
             h_expect, h.data, **self.check_forward_options)
+        testing.assert_allclose(
+            c_prev_data[batch:], c.data[batch:], **self.check_forward_options)
 
     @condition.retry(3)
     def test_forward_cpu(self):

tests/chainer_tests/links_tests/connection_tests/test_lstm.py
@@ -27,33 +27,39 @@ class TestLSTM(unittest.TestCase):
         self.upward = upward.copy()  # fixed on CPU
         self.lateral = lateral.copy()  # fixed on CPU
 
-        x_shape = (4, self.in_size)
-        self.x = numpy.random.uniform(-1, 1, x_shape).astype(numpy.float32)
+        x1_shape = (4, self.in_size)
+        self.x1 = numpy.random.uniform(-1, 1, x1_shape).astype(numpy.float32)
+        x2_shape = (3, self.in_size)
+        self.x2 = numpy.random.uniform(-1, 1, x2_shape).astype(numpy.float32)
 
-    def check_forward(self, x_data):
+    def check_forward(self, x1_data, x2_data):
         xp = self.link.xp
-        x = chainer.Variable(x_data)
-        h1 = self.link(x)
-        c0 = chainer.Variable(xp.zeros((len(self.x), self.out_size),
-                                       dtype=self.x.dtype))
-        c1_expect, h1_expect = functions.lstm(c0, self.link.upward(x))
+        x1 = chainer.Variable(x1_data)
+        h1 = self.link(x1)
+        c0 = chainer.Variable(xp.zeros((len(self.x1), self.out_size),
+                                       dtype=self.x1.dtype))
+        c1_expect, h1_expect = functions.lstm(c0, self.link.upward(x1))
         testing.assert_allclose(h1.data, h1_expect.data)
         testing.assert_allclose(self.link.h.data, h1_expect.data)
         testing.assert_allclose(self.link.c.data, c1_expect.data)
 
-        h2 = self.link(x)
+        batch = len(x2_data)
+        x2 = chainer.Variable(x2_data)
+        h2 = self.link(x2)
+        h1_in, h1_rest = functions.split_axis(h1, [batch], axis=0)
         c2_expect, h2_expect = \
             functions.lstm(c1_expect,
-                           self.link.upward(x) + self.link.lateral(h1))
-        testing.assert_allclose(h2.data, h2_expect.data)
+                           self.link.upward(x2) + self.link.lateral(h1_in))
+        testing.assert_allclose(h2.data[:batch], h2_expect.data)
+        testing.assert_allclose(h2.data[batch:], h1_rest.data)
 
     def test_forward_cpu(self):
-        self.check_forward(self.x)
+        self.check_forward(self.x1, self.x2)
 
     @attr.gpu
     def test_forward_gpu(self):
         self.link.to_gpu()
-        self.check_forward(cuda.to_gpu(self.x))
+        self.check_forward(cuda.to_gpu(self.x1), cuda.to_gpu(self.x2))
 
 
 class TestLSSTMRestState(unittest.TestCase):